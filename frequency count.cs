3ea7ea2dae1aa9797f137c810689885be4e8a9b3
unknown <bhaktidukare14@gmail.com>
2023-03-16 18:18:25 +0530

    Frequency count of string file added

---
+++ frequency count.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Frequency
+{
+    public static void Main()
+    {
+        String str = "Hello world";
+        int[] freq = new int[str.Length];
+        int i, j;
+        char[] ch = str.ToCharArray();
+
+        for (i = 0; i < str.Length; i++)
+        {
+            freq[i] = 1;
+            for (j = i + 1; j < str.Length; j++)
+            {
+                if (ch[i] == ch[j])
+                {
+                    freq[i]++;
+                    ch[j] = '0';
+                }
+            }
+        }
+        for (i = 0; i < freq.Length; i++)
+        {
+            if (ch[i] != ' ' && ch[i] != '0')
+                Console.WriteLine(ch[i] + "-" + freq[i]);
+        }
+        
+    }
+}
